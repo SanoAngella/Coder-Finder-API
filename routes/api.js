d2633eb9f4b277093e24f227acb84d05d196f115
Angella <sanoangella9@gmail.com>
2026-02-01 21:52:11 +0200

    Refactor API routes for coders management

--- routes/api.js
+++ routes/api.js
@@ -1,36 +1,45 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const express = require('express');
+const router = express.Router();
+const Coder = require('../models/coder');
 
+router.get('/coders', function(req, res, next) {
+    const lng = parseFloat(req.query.lng);
+    const lat = parseFloat(req.query.lat);
 
+    if (isNaN(lng) || isNaN(lat)) {
+        return res.status(400).send({ error: "Please provide lng and lat" });
+    }
 
-const GeoSchema = new Schema({
-  type:{
-    type: String,
-    default: "Point"
-  },
-  coordinates: {
-    type: [Number],
-    index: "2dsphere"
-  }
-})
-
-//Create coder Schema & model
-const CoderSchema = new Schema({
-    name:{
-        type: String,
-        required: [true, 'Name field is required']
-    },
-    prof: {
-        type: String, 
-    },
-    available: {
-        type: Boolean,
-        default: false
-    },
-    
-    //add in geo location
- geometry: GeoSchema
-
+    Coder.aggregate([
+        {
+            $geoNear: {
+                near: { type: 'Point', coordinates: [lng, lat] },
+                distanceField: "dist.calculated",
+                maxDistance: 100000,
+                spherical: true
+            }
+        }
+    ]).then(function(coders) {
+        res.send(coders);
+    }).catch(next);
 });
-const Coder= mongoose.model('coder', CoderSchema);
-module.exports = Coder;
+
+router.post('/coders', function(req, res, next) {
+    Coder.create(req.body).then(function(coder) {
+        res.send(coder);
+    }).catch(next);
+});
+
+router.put('/coders/:id', function(req, res, next) {
+    Coder.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true }).then(function(coder) {
+        res.send(coder);
+    }).catch(next);
+});
+
+router.delete('/coders/:id', function(req, res, next) {
+    Coder.findByIdAndDelete({ _id: req.params.id }).then(function(coder) {
+        res.send(coder);
+    }).catch(next);
+});
+
+module.exports = router;
